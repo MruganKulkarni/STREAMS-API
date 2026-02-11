6c6e60c0e4bc69493289d17974510381eee460a7
MruganKulkarni <mrugan.kulkarni63@gmail.com>
2026-02-11 16:08:22 +0530

    [Mrugan Kulkarni] : UC2.1 - Create stream and print elements using forEach

---
+++ src/uc21/StreamForEach.java
@@ -0,0 +1,15 @@
+package uc2_1;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamForEach {
+
+    public static void main(String[] args) {
+
+        List<Integer> numbers = Arrays.asList(10, 20, 30, 40, 50);
+
+        numbers.stream()
+                .forEach(System.out::println);
+    }
+}
