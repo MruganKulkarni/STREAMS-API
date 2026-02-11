4050fc60cfe89a85f846e75c7c0c4b9ab6bb3fb3
MruganKulkarni <mrugan.kulkarni63@gmail.com>
2026-02-11 16:13:12 +0530

    [Mrugan Kulkarni] : UC2.4 - Filter even numbers using filter()

---
+++ src/uc24/FilterEven.java
@@ -0,0 +1,16 @@
+package uc24;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FilterEven {
+
+    public static void main(String[] args) {
+
+        List<Integer> numbers = Arrays.asList(10, 15, 20, 25, 30, 35);
+
+        numbers.stream()
+                .filter(n -> n % 2 == 0)
+                .forEach(System.out::println);
+    }
+}
