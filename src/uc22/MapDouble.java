2acf9df00a5798273093265f23ee6bcec1acb6b8
MruganKulkarni <mrugan.kulkarni63@gmail.com>
2026-02-11 16:09:56 +0530

    [Mrugan Kulkarni] : UC2.2 - Transform elements using map()

---
+++ src/uc22/MapDouble.java
@@ -0,0 +1,16 @@
+package uc22;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MapDouble {
+
+    public static void main(String[] args) {
+
+        List<Integer> numbers = Arrays.asList(10, 20, 30, 40, 50);
+
+        numbers.stream()
+                .map(n -> n * 2)
+                .forEach(System.out::println);
+    }
+}
